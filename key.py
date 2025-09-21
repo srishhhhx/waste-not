5aaad5e9f64c5fd2effe8132d550b7a7f405056b
Srishan <srish18srishan@gmail.com>
2025-09-21 19:51:43 +0530

    Key changes

--- key.py
+++ key.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"gcloud-key.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcloud-key.json"
 from google.cloud import vision
 
 client = vision.ImageAnnotatorClient()
